159fd3a62b65d36c81e249cbcf5119357ba5679f
Andrey-smol <andreyksmol@yandex.ru>
2025-04-12 11:35:51 +0300

    max range of values was added

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,18 +1,35 @@
 package ru.netology;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
-        inefficientAlgorithm(texts);
-        moreEfficientAlgorithm(texts);
+        long startTs = System.currentTimeMillis(); // start time
+
+        List<Future<Integer>> futureList;
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        futureList = executorService.invokeAll(Arrays.stream(texts).map(MyCallable::new).collect(Collectors.toList()));
+        int max = 0;
+        for (Future<Integer> future : futureList) {
+            max = Math.max(max, future.get());
+        }
+        System.out.println("Максимальный интервал значений = " + max);
+
+        long endTs = System.currentTimeMillis(); // end time
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+        executorService.shutdown();
     }
 
     public static String generateText(String letters, int length) {
@@ -22,68 +39,5 @@ public class Main {
             text.append(letters.charAt(random.nextInt(letters.length())));
         }
         return text.toString();
-    }
-
-    public static void inefficientAlgorithm(String[] texts) {
-        long startTs = System.currentTimeMillis(); // start time
-        for (String text : texts) {
-            int maxSize = 0;
-            for (int i = 0; i < text.length(); i++) {
-                for (int j = 0; j < text.length(); j++) {
-                    if (i >= j) {
-                        continue;
-                    }
-                    boolean bFound = false;
-                    for (int k = i; k < j; k++) {
-                        if (text.charAt(k) == 'b') {
-                            bFound = true;
-                            break;
-                        }
-                    }
-                    if (!bFound && maxSize < j - i) {
-                        maxSize = j - i;
-                    }
-                }
-            }
-            System.out.println(text.substring(0, 100) + " -> " + maxSize);
-        }
-        long endTs = System.currentTimeMillis(); // end time
-        System.out.println("Time: " + (endTs - startTs) + "ms");
-    }
-
-    public static void moreEfficientAlgorithm(String[] texts) throws InterruptedException {
-        long startTs = System.currentTimeMillis(); // start time
-        List<Thread> threadList = new ArrayList<>();
-
-        for (String text : texts) {
-            Thread thread = new Thread(() -> {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
-                            }
-                        }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
-                    }
-                }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-            });
-            threadList.add(thread);
-            thread.start();
-        }
-        for (Thread thread : threadList) {
-            thread.join();
-        }
-        long endTs = System.currentTimeMillis(); // end time
-        System.out.println("Time: " + (endTs - startTs) + "ms");
     }
 }
